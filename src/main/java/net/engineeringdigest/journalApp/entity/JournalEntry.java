c245a12f9ef05f04df2514f1df9015cac450e954
Akshay-Reddy00 <akshayreddym19@gmail.com>
2024-08-16 01:36:06 -0400

    Added User Entity along with Controller

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.entity;
 
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -17,6 +18,7 @@ public class JournalEntry {
 
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
--- src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -2,22 +2,32 @@ package net.engineeringdigest.journalApp.entity;
 
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Document(collection="journal_entries")
+@Document(collection="users")
 @Getter
 @Setter
 public class User {
 
     @Id
     private ObjectId id;
-    private String title;
-    private String content;
-    private LocalDateTime date;
+
+    @Indexed(unique = true)
+    @NonNull
+    private String userName;
+    @NonNull
+    private String password;
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
 
 }
